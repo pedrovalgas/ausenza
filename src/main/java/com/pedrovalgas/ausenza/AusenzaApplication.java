4143e5987de86a6837ee629da39ee5c03142a498
Pedro Lucas <plpvalgas@gmail.com>
2025-12-09 11:36:31 -0300

    feat: setup inicial com docker e ci

---
+++ src/main/java/com/pedrovalgas/ausenza/AusenzaApplication.java
@@ -0,0 +1,13 @@
+package com.pedrovalgas.ausenza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AusenzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AusenzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrovalgas/ausenza/AusenzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrovalgas.ausenza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AusenzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
